f41c35ccb8350ffa10c7defffef3eb58994d70ea
wxuhao32 <wxuhao32@gmail.com>
2026-05-01 21:36:17 +0800

    fix: 将前端路由从History模式切换为Hash模式，解决部署后刷新页面404问题

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import { ElMessage } from 'element-plus'
 
@@ -130,7 +130,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
